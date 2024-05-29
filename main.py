41f978c73cfd196fd465e8e1a33872139d00311f
Micah Russell <108246578+MORImementos@users.noreply.github.com>
2024-05-29 14:02:03 -0500

    Add make transparent function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from PIL import Image
 import json
+import fnmatch
 
 original_images = []
 new_images = []
@@ -9,6 +10,7 @@ new_images = []
 orig_path = "original_images/"
 new_path = "new_images/"
 output_path = "output/"
+make_transparent_path = "make_transparent/"
 
 ##### ADJUST MAX WIDTH AND HEIGHT HERE #####
 MAX_WIDTH = 4000
@@ -27,6 +29,15 @@ with open("image_key.json") as dt:
 # get all filenames in a directory (in this case, the original images and the new images)
 def get_filenames(directory):
     return [file for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file))]
+
+def get_unique_filenames(output_path, filename):
+    base, ext = os.path.splitext(filename)
+    counter = 1
+    new_filename = filename
+    while os.path.exists(os.path.join(output_path, new_filename)):
+        new_filename = f"{base}_{counter}{ext}"
+        counter += 1
+    return new_filename
 
 
 # find instances where the new files and the original files match (to know which ones are going to be used)
@@ -62,7 +73,8 @@ def resize_new_images(orig_sizes, new_images, keywords, multiplier=10):
             # else if the image has the same name as original file
             elif orig_filename in new_images:
                 new_file = orig_filename
-
+            # Find all files in new_images that contain the keyword
+            # matching_files = fnmatch.filter(new_images, f"*{keyword}*")
             # open image
             with Image.open(os.path.join(new_path, new_file)) as im:
                 print(f"Resizing {new_file}. . .")
@@ -88,9 +100,29 @@ def resize_new_images(orig_sizes, new_images, keywords, multiplier=10):
                         multiplier -= 1
 
 
+def make_images_transparent(transparent_sizes, transparent_images):
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    for filename in transparent_images:
+        # print(filename, orig_sizes)
+        # this logic should likely be more fleshed out, but for now, it will just automatically include this in the new,
+        # without need for the original file. todo: filename validation of some form
+        # if filename in orig_sizes:
+        trans_width, trans_height = transparent_sizes[filename]
+        transparent_img = Image.new("RGBA", (trans_width, trans_height), (0, 0, 0 ,0))
+        transparent_img.save(os.path.join(output_path, filename))
+        print(f"{filename} made transparent.")
+
 if __name__ == "__main__":
     original_images = get_filenames(orig_path)
     new_images = get_filenames(new_path)
+    transparent_images = get_filenames(make_transparent_path)
+
     orig_sizes = get_image_sizes(orig_path, original_images)
     new_sizes = get_image_sizes(new_path, new_images)
+    transparent_sizes = get_image_sizes(make_transparent_path, transparent_images)
+
     resize_new_images(orig_sizes, new_images, keywords, multiplier=multiplier)
+    make_images_transparent(transparent_sizes, transparent_images)
+
